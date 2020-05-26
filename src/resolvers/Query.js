51e5ffb086a6d9fc84c10a79cf5fc298b13a3110
Nathaniel Mosco <natemosco@gmail.com>
2020-05-26 02:08:33 -0400

    limit-offset filtering for pagination sorting by link creationtime link url lexically link description lexically

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -3,7 +3,30 @@ function info() {
 }
 
 function feed(root, args, context, info) {
-    return context.prisma.links();
+    const where = args.filter ? {
+        OR: [
+            { description_contains: args.filter },
+            { url_contains: args.filter },
+        ]
+    } : {}
+
+    const links = await context.prisma.links({
+        where,
+        skip: args.skip,
+        first: args.first,
+        orderBy: args.orderBy
+    })
+
+    const count = await context.prisma.linksConnection({
+        where,
+    })
+    .aggregate()
+    .count()
+
+    return {
+        links,
+        count,
+    }
 }
 
 module.exports = {
