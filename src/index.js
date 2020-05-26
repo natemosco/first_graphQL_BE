61b26249a51006ee8627285f7c79c5be8b8c60d0
Nathaniel Mosco <natemosco@gmail.com>
2020-05-26 00:41:28 -0400

    subscriptions added for voting

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const Mutation = require("./resolvers/Mutation");
 const User = require("./resolvers/User");
 const Link = require("./resolvers/Link");
 const Subscription = require("./resolvers/Subscription");
+const Vote = require("./resolvers/Vote");
 
 const resolvers = {
     Query,
@@ -12,6 +13,7 @@ const resolvers = {
     User,
     Link,
     Subscription,
+    Vote,
 };
 
 const server = new GraphQLServer({
